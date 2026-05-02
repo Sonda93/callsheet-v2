30df05f42d20be4ea6c492551db13f42e82bfa57
Sonda93 <119947996+Sonda93@users.noreply.github.com>
2026-05-02 14:43:04 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,17 +6,23 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
     })
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
